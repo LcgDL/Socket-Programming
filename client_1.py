c7641dd490b3458b245c74e88408d24108f639c0
Leo C_G <84103303+LcgDL@users.noreply.github.com>
2021-10-28 13:34:22 +0200

    Update client_1.py

--- client_1.py
+++ client_1.py
@@ -3,7 +3,7 @@ import socket
 
 s = socket.socket()
 hostServer = 'ip_server'
-port = 'port_server'
+port = port_server
 s.connect((hostServer, port))
 m = input(('Write a message and press enter: \n'))
 s.sendall(m.encode())    
